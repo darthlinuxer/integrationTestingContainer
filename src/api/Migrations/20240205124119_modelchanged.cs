3e68eb5ca9faa69ca4a41a36987c15b2bdd6e519
darthlinuxer <chaves.camilo@gmail.com>
2024-02-05 09:46:01 -0300

    BugFix in Migrations

---
+++ src/api/Migrations/20240205124119_modelchanged.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace example.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class modelchanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Person",
+                newName: "Email");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Person",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SurName",
+                table: "Person",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "SurName",
+                table: "Person");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Person",
+                newName: "Name");
+        }
+    }
+}
